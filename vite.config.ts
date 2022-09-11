01a13b7b103df8f323d5d72406fb297e2696f120
fernandoprestes <fernandoprestesc@gmail.com>
2022-09-11 15:19:28 -0300

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 
 const PATH_SRC = path.resolve(__dirname, './src');
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 4000,
